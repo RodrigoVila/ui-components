26ba4516b4ebb1739986ed300f78696772a8753c
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2024-02-01 01:07:11 +0100

    Add tailwind & linft configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,37 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    "plugin:prettier/recommended"
+  ],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        ".eslintrc.{js,cjs}"
+      ],
+      parserOptions: {
+        "sourceType": "script"
+      }
+    }
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', "@typescript-eslint"],
   rules: {
+    "react/react-in-jsx-scope": "off",
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    "prettier/prettier": [
+      "error",
+      {
+        "endOfLine": "auto",
+        "singleQuote": true,
+      }
+    ]
   },
+
 }
